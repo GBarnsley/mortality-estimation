6cf76065fa9b8ac7e67f6f30d17757d220d220f9
GBarnsley <63357847+GBarnsley@users.noreply.github.com>
2026-02-21 00:59:26 +0000

    almost match

--- analysis/estimate_basic.jl
+++ analysis/estimate_basic.jl
@@ -56,16 +56,30 @@ struct VRCMortModel{S, A, R, T_pre, T_post}
     conflict_pre::Array{Float64, 4}
     conflict_post::Array{Float64, 4}
     #options
-    region_random_effects_mortality::Bool
-    region_random_effects_reporting::Bool
+    regional_random_effects_mortality::Bool
+    regional_random_effects_reporting::Bool
     under_reporting_pre::Bool
+    national_random_walk_mortality::Bool
+    national_random_walk_reporting::Bool
+    regional_random_walk_mortality::Bool
+    regional_random_walk_reporting::Bool
+    conflict_pooling_mortality::Bool
+    conflict_pooling_reporting::Bool
+    age_selective_drop::Bool
     #priors (some named tuple?)
     function VRCMortModel(
         y_pre, y_post, exposure_pre, exposure_post, conflict_pre, conflict_post;
-        region_random_effects_mortality = true,
-        region_random_effects_reporting = true,
+        regional_random_effects_mortality = true,
+        regional_random_effects_reporting = true,
         under_reporting_pre = false,
-        standardize_covariates = true
+        standardize_covariates = true,
+        national_random_walk_mortality = true,
+        national_random_walk_reporting = true,
+        regional_random_walk_mortality = true,
+        regional_random_walk_reporting = true,
+        conflict_pooling_mortality = true,
+        conflict_pooling_reporting = true,
+        age_selective_drop = true
     )
         S, A, R, T_pre = size(y_pre)
         T_post = size(y_post, 4)
@@ -88,9 +102,16 @@ struct VRCMortModel{S, A, R, T_pre, T_post}
             log.(exposure_post),
             conflict_pre,
             conflict_post,
-            region_random_effects_mortality,
-            region_random_effects_reporting,
-            under_reporting_pre
+            regional_random_effects_mortality,
+            regional_random_effects_reporting,
+            under_reporting_pre,
+            national_random_walk_mortality,
+            national_random_walk_reporting,
+            regional_random_walk_mortality,
+            regional_random_walk_reporting,
+            conflict_pooling_mortality,
+            conflict_pooling_reporting,
+            age_selective_drop
         )
     end
 end
@@ -122,21 +143,47 @@ end
     return nothing
 end
 
+function square(x::T) where {T <: Real}
+    x * x
+end
+
 @model function mortality_regression(vrc_mort_model::VRCMortModel{S, A, R, T_pre, T_post}) where {S, A, R, T_pre, T_post}
-    (; conflict_pre, conflict_post, region_random_effects_mortality) = vrc_mort_model
+    (; conflict_pre, conflict_post, regional_random_effects_mortality, national_random_walk_mortality,
+        regional_random_walk_mortality, conflict_pooling_mortality) = vrc_mort_model
     #mortality parameters
     α₀ ~ Normal(-9, 2)
     αₛ ~ filldist(Normal(0, 0.5), S)
     αₐ ~ filldist(Normal(0, 1), 1, A)
-    β_conf ~ filldist(truncated(Normal(0.2, 0.3); lower = 0.0), 1, 1, R)
+
+    if conflict_pooling_mortality
+        β_conf_cental ~ truncated(Normal(0.2, 0.3); lower = 0.0)
+        σᵦ_conf ~ truncated(Normal(0, 0.3); lower = 0.0)
+        β_conf_raw ~ filldist(Normal(0, 1), 1, 1, R)
+
+        β_conf = β_conf_cental .* exp.(σᵦ_conf .* (β_conf_raw .- mean(β_conf_raw)) .- (0.5 .* square(σᵦ_conf)))
+    else
+        β_conf ~ truncated(Normal(0.2, 0.3); lower = 0.0)
+    end
 
     log_λ_pre = α₀ .+ αₛ .+ αₐ .+ (β_conf .* conflict_pre)
     log_λ_post = α₀ .+ αₛ .+ αₐ .+ (β_conf .* conflict_post)
 
-    if region_random_effects_mortality
+    if regional_random_effects_mortality
         uᵣ ~ to_submodel(region_random_effects(vrc_mort_model))
         log_λ_pre .+= uᵣ
         log_λ_post .+= uᵣ
+    end
+
+    if national_random_walk_mortality
+        random_walk ~ to_submodel(national_random_walk(vrc_mort_model))
+        log_λ_pre .+= random_walk.v_pre
+        log_λ_post .+= random_walk.v_post
+    end
+
+    if regional_random_walk_mortality
+        random_walk_region ~ to_submodel(regional_random_walk(vrc_mort_model))
+        log_λ_pre .+= random_walk_region.v_pre
+        log_λ_post .+= random_walk_region.v_post
     end
 
     return (;
@@ -146,10 +193,21 @@ end
 end
 
 @model function reporting_regression(vrc_mort_model::VRCMortModel{S, A, R, T_pre, T_post}) where {S, A, R, T_pre, T_post}
-    (; conflict_post, region_random_effects_reporting, under_reporting_pre) = vrc_mort_model
+    (; conflict_post, regional_random_effects_reporting, under_reporting_pre, national_random_walk_reporting, 
+        regional_random_walk_reporting, conflict_pooling_reporting, age_selective_drop) = vrc_mort_model
     #reporting parameters
     κ₀ ~ Normal(2.197, 1.0) #logit(0.9)
-    γ_conf ~ Normal(0, 0.3) #reporting conflict effect
+    
+    if conflict_pooling_reporting
+        γ_conf_cental ~ truncated(Normal(0, 0.3); lower = 0.0)
+        σᵧ_conf ~ truncated(Normal(0, 0.5); lower = 0.0)
+        γ_conf_raw ~ filldist(Normal(0, 1), 1, 1, R)
+
+        γ_conf = γ_conf_cental .+ σᵧ_conf .* (γ_conf_raw .- mean(γ_conf_raw))
+    else
+        γ_conf ~ truncated(Normal(0, 0.3); lower = 0.0)
+    end
+    
     logit_ρ_post = κ₀ .+ (γ_conf .* conflict_post)
 
     if under_reporting_pre
@@ -161,13 +219,39 @@ end
         logit_ρ_post .+= κ_post
     end
 
-    if region_random_effects_reporting
+    if regional_random_effects_reporting
         uᵣ ~ to_submodel(region_random_effects(vrc_mort_model))
         logit_ρ_post .+= uᵣ
 
         if under_reporting_pre
             logit_ρ_pre .+= uᵣ
         end
+    end
+
+    if national_random_walk_reporting
+        random_walk ~ to_submodel(national_random_walk(vrc_mort_model; include_pre = vrc_mort_model.under_reporting_pre))
+        logit_ρ_post .+= random_walk.v_post
+
+        if under_reporting_pre
+            logit_ρ_pre .+= random_walk.v_pre
+        end
+    end
+
+    if regional_random_walk_reporting
+        random_walk_regional ~ to_submodel(regional_random_walk(vrc_mort_model; include_pre = vrc_mort_model.under_reporting_pre))
+        logit_ρ_post .+= random_walk_regional.v_post
+
+        if under_reporting_pre
+            logit_ρ_pre .+= random_walk_regional.v_pre
+        end
+    end
+
+    if age_selective_drop
+        #probably a better way of structuring this
+        δ_incr ~ filldist(truncated(Normal(0, 0.5); lower = 0.0), 1, A - 1)
+        δ_scale ~ truncated(Normal(0, 1); lower = 0.0)
+        δ = hcat(zero(δ_scale), cumsum(δ_incr; dims = 2) .* δ_scale)
+        logit_ρ_post = logit_ρ_post .- δ
     end
 
     #make logscale
@@ -187,12 +271,56 @@ end
 
 @model function region_random_effects(vrc_mort_model::VRCMortModel{S, A, R, T_pre, T_post}) where {S, A, R, T_pre, T_post}
     u_raw ~ filldist(Normal(0, 1), 1, 1, R) #region random effect
-    sigma_u ~ truncated(Normal(0, 0.5), lower = 0.0)
-    return ((u_raw .- mean(u_raw)) .* sigma_u) 
+    σ ~ truncated(Normal(0, 0.5), lower = 0.0)
+    return ((u_raw .- mean(u_raw)) .* σ) 
+end
+
+@model function national_random_walk(vrc_mort_model::VRCMortModel{S, A, R, T_pre, T_post}; include_pre = true) where {S, A, R, T_pre, T_post}
+    ϵ_post ~ filldist(Normal(0, 1), 1, 1, 1, T_post)
+    σ ~ truncated(Normal(0, 0.2), lower = 0.0)
+    
+    if include_pre
+        ϵ_pre ~ filldist(Normal(0, 1), 1, 1, 1, T_pre)
+
+        v_pre = cumsum(ϵ_pre .* σ, dims = 4)
+        v_post = cumsum((ϵ_post .* σ) .+ v_pre[:, :, :, end], dims = 4)
+        v_mean = (sum(v_pre) + sum(v_post)) / (T_pre + T_post)
+        v_pre .-= v_mean
+        v_post .-= v_mean
+
+        return (; v_pre = v_pre, v_post = v_post)
+    else
+        v_post = cumsum((ϵ_post .* σ), dims = 4)
+        v_post .-= mean(v_post)
+
+        return (; v_post = v_post)
+    end
+end
+
+@model function regional_random_walk(vrc_mort_model::VRCMortModel{S, A, R, T_pre, T_post}; include_pre = true) where {S, A, R, T_pre, T_post}
+    ϵ_post ~ filldist(Normal(0, 1), 1, 1, R, T_post)
+    σ ~ truncated(Normal(0, 0.2), lower = 0.0)
+    
+    if include_pre
+        ϵ_pre ~ filldist(Normal(0, 1), 1, 1, R, T_pre)
+
+        v_pre = cumsum(ϵ_pre .* σ, dims = 4)
+        v_post = cumsum((ϵ_post .* σ) .+ v_pre[:, :, :, end], dims = 4)
+        v_mean = (sum(v_pre, dims = 4) .+ sum(v_post, dims = 4)) ./ (T_pre + T_post)
+        
+        v_pre .-= v_mean
+        v_post .-= v_mean
+
+        return (; v_pre = v_pre, v_post = v_post)
+    else
+        v_post = cumsum((ϵ_post .* σ), dims = 4)
+        v_post .-= mean(v_post, dims = 4)
+
+        return (; v_post = v_post)
+    end
 end
 
 @model function vrc_model(vrc_mort_model::VRCMortModel{S, A, R, T_pre, T_post}; y_pre = vrc_mort_model.y_pre, y_post = vrc_mort_model.y_post) where {S, A, R, T_pre, T_post}
-#@model function vrc_model(y_pre, y_post, vrc_mort_model::VRCMortModel{S, A, R, T_pre, T_post}) where {S, A, R, T_pre, T_post}
     #dispersion
     ϕ ~ Exponential(1.0)
 
@@ -225,15 +353,15 @@ vrc_mort_model = VRCMortModel(
     exposure_post,
     conflict_pre,
     conflict_post;
-    region_random_effects_mortality = true,
-    region_random_effects_reporting = true,
+    regional_random_effects_mortality = true,
+    regional_random_effects_reporting = true,
     under_reporting_pre = false,
     standardize_covariates = true
 )
 
 model = vrc_model(vrc_mort_model)
 
-result = sample(model, NUTS(; adtype = AutoMooncake()), 10)
+result = sample(model, NUTS(; adtype = AutoMooncake()), MCMCThreads(), 1000, 3)
 
 names(result, :parameters)
 
